ed61e7ddd0e16d24e390b93549c64e5128f6fd4d
nknandakuamr <tec.thunder2022nk@gmail.com>
2025-03-27 23:56:08 +0530

    100%

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 class="text-3xl text-center font-bold underline">
+    React + Tailwind CSS Starter Template.
+  </h1>
+  )
+}
+
+export default App
